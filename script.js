6bfeed53ad2d46b8e3d17afe1224170a385aec49
pengin12 <137031551+pengin12@users.noreply.github.com>
2023-10-07 18:53:16 +0700

    Add function to draw and border for grid element

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16*16; i++) {
-  const i = document.createElement('div');
-  i.style.height = "30px";
-  i.style.width = "30px";
-  container.appendChild(i);
+
+for (let i = 0; i < 16 * 16; i++) {
+  let div = document.createElement("div");
+  div.classList.add("grid_element");
+  div.addEventListener("dragover", function() {
+    div.style.backgroundColor = "black";
+  });
+  div.addEventListener("click", function() {
+    div.style.backgroundColor = "black";
+  });
+
+  container.appendChild(div);
 }
+
+container.focus();
