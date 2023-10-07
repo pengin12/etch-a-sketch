fba31ad7b43f38eaf31fd6493e76e410b225db9e
pengin12 <137031551+pengin12@users.noreply.github.com>
2023-10-07 19:54:35 +0700

    Remove grid element border to add a function that change grid size

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 16 * 16; i++) {
   let div = document.createElement("div");
+  div.style.height = "28px";
+  div.style.width = "28px";
   div.classList.add("grid_element");
   div.addEventListener("dragover", function() {
     div.style.backgroundColor = "black";
@@ -14,3 +16,27 @@ for (let i = 0; i < 16 * 16; i++) {
 }
 
 container.focus();
+
+function grid_size() {
+  let gridSize = prompt("Enter grid size (maximum is 64):");
+  if (gridSize <= 64) {
+    while (container.lastChild) {
+      container.removeChild(container.lastChild);
+    }
+    for (let i = 0; i < gridSize*gridSize; i++) {
+      let div = document.createElement("div");
+      div.style.height = `calc(480px / ${gridSize})`;
+      div.style.width = `calc(480px / ${gridSize})`;
+      div.classList.add("grid_element");
+      div.addEventListener("dragover", function() {
+        div.style.backgroundColor = "black";
+      });
+      div.addEventListener("click", function() {
+        div.style.backgroundColor = "black";
+      });
+
+      container.appendChild(div);
+    }
+  }
+}
+
