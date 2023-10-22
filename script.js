e38ee80d9354153f57752e7352dc75c00c3eddda
pengin12 <137031551+pengin12@users.noreply.github.com>
2023-10-22 13:32:23 +0700

    Add rainbow function

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  let div = document.createElement("div");
-  div.style.height = "30px";
-  div.style.width = "30px";
-  div.classList.add("grid_element");
-  div.addEventListener("dragover", function() {
-    div.style.backgroundColor = "black";
-  });
-  div.addEventListener("click", function() {
-    div.style.backgroundColor = "black";
-  });
-
-  container.appendChild(div);
-}
-
-container.focus();
-
 function grid_size() {
   let gridSize = prompt("Enter grid size (maximum is 64):");
   if (gridSize <= 64) {
@@ -40,3 +23,66 @@ function grid_size() {
   }
 }
 
+function randomColor() {
+  let red = Math.floor(Math.random() * (255 + 1));
+  let green = Math.floor(Math.random() * (255 + 1));
+  let blue = Math.floor(Math.random() * (255 + 1));
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function rainbow() {
+  if (document.querySelector("input[type=checkbox]").checked) {
+    document.querySelectorAll(".grid_element").forEach(function(element) {
+      element.removeEventListener("click", function() {
+        element.style.backgroundColor = "black"; 
+      })
+      element.removeEventListener("dragover", function() {
+        element.style.backgroundColor = "black";
+      })
+    });
+    
+    document.querySelectorAll(".grid_element").forEach(function(element) {
+      element.addEventListener("click", function() {
+        element.style.backgroundColor = randomColor();
+      })
+      element.addEventListener("dragover", function() {
+        element.style.backgroundColor = randomColor();
+      })
+    });
+  } else {
+    document.querySelectorAll(".grid_element").forEach(function(element) {
+      element.removeEventListener("click", function() {
+        element.style.backgroundColor = randomColor();
+      })
+      element.removeEventListener("dragover", function() {
+        element.style.backgroundColor = randomColor();
+      })
+    });
+
+    document.querySelectorAll(".grid_element").forEach(function(element) {
+      element.addEventListener("click", function() {
+        element.style.backgroundColor = "black";
+      })
+      element.addEventListener("dragover", function() {
+        element.style.backgroundColor = "black";
+      })
+    });
+  }
+}
+
+for (let i = 0; i < 16 * 16; i++) {
+  let div = document.createElement("div");
+  div.style.height = "30px";
+  div.style.width = "30px";
+  div.classList.add("grid_element");
+  container.appendChild(div);
+}
+
+document.querySelectorAll(".grid_element").forEach(function(element) {
+  element.addEventListener("click", function() {
+    element.style.backgroundColor = "black";
+  })
+  element.addEventListener("dragover", function() {
+    element.style.backgroundColor = "black";
+  })
+});
